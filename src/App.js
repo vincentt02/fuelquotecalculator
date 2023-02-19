4268957131c216937f508d3e2bb3ed85d7d51dc6
ecolijah <ecolijah@gmail.com>
2023-02-18 21:50:56 -0600

    boilerplate created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Login from './assets/Login';
+import FuelQuoteHistory from './assets/FuelQuoteHistory';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <FuelQuoteHistory />
       
     </div>
   );
---
+++ src/assets/FuelQuoteHistory.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FuelQuoteHistory() {
+  return (
+    <div>FuelQuoteHistory</div>
+  )
+}
