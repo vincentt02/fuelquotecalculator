d76700880aebf5fb5e1f6b0e1ec5ac41a237620e
ecolijah <ecolijah@gmail.com>
2023-02-19 15:14:17 -0600

    corrected css file import and targeted the table class in css

--- src/assets/FuelQuoteHistory.jsx
+++ src/assets/FuelQuoteHistory.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../assets/FuelQuoteHistory.css'
 const data = [
   {numG: 20, address: "2701 Helena st", date: "05/16/2001", price: 214, due: 287}
 ]
@@ -7,22 +7,22 @@ const data = [
 
 export default function FuelQuoteHistory() {
   return (
-    <><div>FuelQuoteHistory</div><table>
+    <><div>FuelQuoteHistory</div><table className="fuelTable">
       <tr>
-        <th>Gallons Requested</th>
-        <th>Delivery Address</th>
-        <th>Delivery Date</th>
-        <th>Suggested Price per Gallon</th>
-        <th>Total Amount Due</th>
+        <th className="fuelTableth">Gallons Requested</th>
+        <th className="fuelTableth">Delivery Address</th>
+        <th className="fuelTableth">Delivery Date</th>
+        <th className="fuelTableth">Suggested Price per Gallon</th>
+        <th className="fuelTableth">Total Amount Due</th>
       </tr>
       {data.map((val, key) => {
           return (
             <tr key={key}>
-              <td>{val.numG}</td>
-              <td>{val.address}</td>
-              <td>{val.date}</td>
-              <td>{val.price}</td>
-              <td>{val.due}</td>
+              <td className="fuelTabletd">{val.numG}</td>
+              <td className="fuelTabletd">{val.address}</td>
+              <td className="fuelTabletd">{val.date}</td>
+              <td className="fuelTabletd">{val.price}</td>
+              <td className="fuelTabletd">{val.due}</td>
             </tr>
           )
         })}
