a29dab0322c923a3a0f84603a894acfcc13d48b5
ecolijah <ecolijah@gmail.com>
2023-02-20 14:27:22 -0600

    refreshing css

--- src/assets/FuelQuoteHistory.jsx
+++ src/assets/FuelQuoteHistory.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../assets/FuelQuoteHistory.css'
 const data = [
+  {numG: 20, address: "2701 Helena st", date: "05/16/2001", price: 214, due: 287},
   {numG: 20, address: "2701 Helena st", date: "05/16/2001", price: 214, due: 287}
 ]
 
@@ -9,11 +10,11 @@ export default function FuelQuoteHistory() {
   return (
     <><div>FuelQuoteHistory</div><table className="fuelTable">
       <tr>
-        <th className="fuelTableth">Gallons Requested</th>
-        <th className="fuelTableth">Delivery Address</th>
-        <th className="fuelTableth">Delivery Date</th>
-        <th className="fuelTableth">Suggested Price per Gallon</th>
-        <th className="fuelTableth">Total Amount Due</th>
+        <th className="fuelTableth">Gallons Requested: </th>
+        <th className="fuelTableth">Delivery Address: </th>
+        <th className="fuelTableth">Delivery Date: </th>
+        <th className="fuelTableth">Suggested Price per Gallon: </th>
+        <th className="fuelTableth">Total Amount Due: </th>
       </tr>
       {data.map((val, key) => {
           return (
