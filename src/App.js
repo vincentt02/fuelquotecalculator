30e051fdc3b84625847391d88146df8c1c9393fe
Luke Campbell <mlgk54@gmail.com>
2023-02-22 21:48:59 -0600

    Remodeled Login/Register screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './assets/Login';
+// import Login from './assets/Login';
+import Login2 from './assets/Login2';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      
+      <Login2/>
     </div>
   );
 }
---
+++ src/assets/Login2.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import "../assets/Login2.css"
+
+
+export default function LoginBS() {
+    return (
+  <div className='login_wrapper'>
+      <Form>
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" placeholder="Enter Username" required/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Enter Password" required/>
+      </Form.Group>
+      <div className="buttons">
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+      <Button variant="danger" type="submit">
+        Register
+      </Button>
+      </div>
+    </Form>
+  </div>
+    )
+}
