273be4e690877edbd8c1db72081d4cb69a924cbe
Luke Campbell <mlgk54@gmail.com>
2023-02-22 18:23:44 -0600

    Added login/Register buttons

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -4,14 +4,19 @@ import "../assets/Login.css"
 export default function Login() {
   return (
   <div className='login_wrapper'>
-   <form className='login_form'>
-      <label for="username"> Username:</label><br/>
+    <div className='login_form_wrapper'>
+      <form className='login_form'>
+      <label for="username" > Username:</label><br/>
       <input type="text" id="username" name="username" required/><br/>
       <label for="password"> Password:</label><br/>
       <input type="password" id="password" name="password" required/><br/>
-      <input type="submit" id="login" value="Login"/>
-      <input type="submit" id="register" value="Register"/>
+      <div className="submit_buttons">
+        <input type="submit" id="login" value="Login"/>
+        <input type="submit" id="register" value="Register"/>
+      </div>
    </form>
+   </div>
+   
  </div>
   )
 }
