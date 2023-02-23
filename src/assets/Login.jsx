c63bfffd173e538754ff4377b20f4445fabde980
Vincent <90940496+vincentt02@users.noreply.github.com>
2023-02-22 22:41:36 -0600

    cleaned up files

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import "../assets/Login.css"
+import "../css/Login.css"
 
 
 export default function Login() {
