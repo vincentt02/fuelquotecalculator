1e95269fc4a6759b3cd9976ce97cff8d206dee2c
angel <eduardo.angel5970@gmail.com>
2023-02-22 21:27:19 -0600

    added useEffect

--- src/assets/FuelQuoteForm.jsx
+++ src/assets/FuelQuoteForm.jsx
@@ -13,12 +13,22 @@ const FuelDate = () => {
 }
 
 export default function FuelQuoteForm() {
+  const [gallonsRequested, setGallonsRequested] = useState(0)
+  const [suggestedPrice, setSuggestedPrice] = useState(5)
+  const [amountDue, setAmountDue] = useState(0)
+
+  useEffect (() => {
+    setAmountDue(gallonsRequested * suggestedPrice)
+  }, [gallonsRequested])
+
+
+
   return (
     <div className='FuelQuoteForm_wrapper'>
       <h2>Fuel Quote Form</h2>
       <form className='FQF_form'>
         <label for="numGallons">Gallons Requested:</label> <br />
-        <input type="numeric" id="numGallons" name="numGallons" required/> <br />
+        <input type="numeric" id="numGallons" name="numGallons" onChange={e => setGallonsRequested(e.target.value)} required/> <br />
 
         <label for="address">Delivery Address:</label> <br />
         <textarea type="text" id="address" name="address" value="123 Main St Houston, TX 77001" required/> <br />
@@ -27,10 +37,10 @@ export default function FuelQuoteForm() {
         <FuelDate></FuelDate>
 
         <label for="suggestedPrice">Suggested Price/Gallon:</label> <br />
-        <input type="numeric" id="suggestedPrice" name="suggestedPrice" readonly="true" value="$1" required/> <br />
+        <input type="numeric" id="suggestedPrice" name="suggestedPrice" readonly="true" value={suggestedPrice} required/> <br />
 
         <label for="amountDue">Total Amount Due:</label> <br />
-        <input type="numeric" id="amountDue" name="amountDue" readonly="true" value="helppp" required/> <br />        
+        <input type="numeric" id="amountDue" name="amountDue" readonly="true" value={amountDue} required/> <br />        
 
       </form>
 
