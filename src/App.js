156b896b2911198f16510f70b8a08e5609c326cf
Vincent <90940496+vincentt02@users.noreply.github.com>
2023-02-22 22:19:26 -0600

    Fixed package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ClientProfileManagement from './assets/ClientProfileManagement';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import FuelQuoteForm from './assets/FuelQuoteForm';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <FuelQuoteForm />
       
     </div>
   );
