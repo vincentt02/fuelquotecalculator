f718ee27038f51f1e9969a79b07cb7110ee26f16
Luke Campbell <mlgk54@gmail.com>
2023-03-09 15:36:50 -0600

    LoginModule basics

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const loginModuleRoute = require("./routes/LoginModule")
+app.use("/Login", loginModuleRoute);
+
 app.listen(8080, () => {
     console.log("Server started on port 8080")
 })
