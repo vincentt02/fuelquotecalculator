27d7274b126d09dedd047c80f3e4874697483542
Luke Campbell <mlgk54@gmail.com>
2023-03-11 14:11:31 -0600

    Added validation to the form fields

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const LoginModuleRoute = require("./routes/LoginModule")
 app.use("/Login", LoginModuleRoute);
 
