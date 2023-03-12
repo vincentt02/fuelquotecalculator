cf49ad592a0369c38518c3d4a15830f93d6e70cd
Luke Campbell <mlgk54@gmail.com>
2023-03-11 18:43:10 -0600

    Validation

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -5,18 +5,61 @@ import "../css/Login.css"
 
 
 export default function Login() {
+
+  const [formData, setFormData] = useState({username: "", password: ""});
+  
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name]: value})
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch("/Login", {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if(response.ok) {
+        console.log("successful")
+      } else {
+
+      } 
+    } catch (error) {
+
+    }
+  }
+
     return (
   <div className='login_wrapper'>
       <Form>
       <Form.Group className="mb-3" controlId="username">
         <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" pattern="[A-Za-z0-9!@#$%^&*]{3,100}" 
-        title="Username should not contain any spaces." id="username" required/>
+        <Form.Control 
+        type="text" 
+        placeholder="Enter Username" 
+        pattern="[A-Za-z0-9!@#$%^&*]{3,100}" 
+        title="Username should not contain any spaces."
+        onChange={handleInputChange} 
+        required
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter Password" pattern="[A-Za-z0-9!@#$%^&*]{3,100}" 
-        title="Password should not contain any spaces." id="password"  required/>
+        <Form.Control 
+        type="password" 
+        placeholder="Enter Password" 
+        pattern="[A-Za-z0-9!@#$%^&*]{3,100}" 
+        title="Password should not contain any spaces." 
+        onChange={handleInputChange}
+        required
+        />
       </Form.Group>
       <div className="buttons">
       <Button variant="primary" type="submit">
