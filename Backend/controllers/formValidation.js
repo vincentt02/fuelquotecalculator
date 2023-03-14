3cf40c3d38e0de94112e0becb3194cac02d5ec40
Eli <ecolijah@gmail.com>
2023-03-14 17:47:07 -0500

    pricingModule.js created, just a class

--- Backend/controllers/formValidation.js
+++
@@ -1,9 +0,0 @@
-const Yup = require("yup");
-
-const formSchema  = Yup.object({
-    username: Yup.string().required("User name required.").matches(/^\S*$/, "No spaces."),
-    password: Yup.string().required("Password required.").matches(/^\S*$/, "No spaces.")
-
-});
-
-module.exports = formValidation;
---
+++ Backend/routes/PricingModule.js
@@ -0,0 +1,19 @@
+class pricingModule {
+
+    constructor(numGallons, deliveryAdress, deliveryDate) {
+        this.numGallons = numGallons;
+        this.deliveryAdress = deliveryAdress;
+        this.deliveryDate = deliveryDate;
+    }
+
+    suggestedPrice() {
+        //function that uses the objects parameters to return the suggested price per gallon.
+        const suggestedPPG = null;
+
+
+        return suggestedPPG;
+    }
+}
+
+
+
