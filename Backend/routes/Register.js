1bc6251075de1a89a20caada9ca87dd180be4b9a
Eli <ecolijah@gmail.com>
2023-03-14 17:31:16 -0500

    validation done on both ends

--- Backend/routes/Register.js
+++ Backend/routes/Register.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(express.json())
-//need to create a new user objects and prepare it for insertion into the DB
-router.post('/', (req, res) =>{
-    console.log("post route running.");
-    
-    const { username, password } = req.body;
-    
-    res.status(201).json({ message: `Registration successful, username: ${username}, password: ${password}`});
-    //res.send(`Welcome ${username}, your account was created with the password ${password}!`);
+router.use(express.json());
+
+router.post('/', (req, res) => {
+  console.log('post route running.');
+
+  const { username, password } = req.body;
+  //array for all error messages that exist.
+  const errors = [];
+
+  // backend validations
+  if (!username.trim()) {
+    errors.push('Username is required.');
+  }
+  if (username.includes(' ')) {
+    errors.push('Username cannot contain spaces.');
+  }
+  if (!password.trim()) {
+    errors.push('Password is required.');
+  }
+  if (password.includes(' ')) {
+    errors.push('Password cannot contain spaces.');
+  }
+
+  if (errors.length > 0) {
+    res.status(400).json({ errors });
+  } else {
+    // prepare new user object for insertion into DB here
+    res.status(201).json({ message: 'Registration successful' });
+  }
 });
 
-
-
-//exporting the module: keep at bottom
 module.exports = router;
