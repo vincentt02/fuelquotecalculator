1f919a1648668b05d5fae5b02c5f4b55fe7dc390
Eli <ecolijah@gmail.com>
2023-03-19 13:54:41 -0500

    cleaning up

--- src/assets/QuoteTable.jsx
+++ src/assets/QuoteTable.jsx
@@ -1,10 +1,13 @@
 import Table from 'react-bootstrap/Table';
-
+import { useEffect } from 'react';
 
 const data = [
   {numG: 20, address: "2701 Helena st", date: "05/16/2001", price: 2.14, due: 42.8},
   {numG: 456, address: "3552 travis St", date: "02/10/2001", price: 3.24, due: 1477.4}
 ]
+
+
+
 function QuoteTable() {
   return ( 
     <Table striped bordered hover>
