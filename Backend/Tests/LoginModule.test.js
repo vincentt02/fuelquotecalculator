4af4b849c7e79858cff9a58bc9a9bf9e5c9ea823
Luke Campbell <mlgk54@gmail.com>
2023-03-19 20:25:28 -0500

    Added Unit Testing for Login Module

--- Backend/Tests/LoginModule.test.js
+++ Backend/Tests/LoginModule.test.js
@@ -10,27 +10,27 @@ app.use("/", LoginModuleRouter);
 describe ('POST /', () => {
 
 //Testing for a valid form
-it("should return 201 status code and 'Login Successful' message", async () => {
+/*it("should return 201 status code and 'Login Successful' message", async () => {
     const response = await supertest(app)
     .post('/')
     .send({username: "Yellow123", password: "Hello1234"});
     expect(response.status).toBe(201);
     expect(response.body.message).toBe('Login successful');
     });
-}); 
+});*/
 
 
 //Testing for an invalid form
-/*it("should return a 400 status code and error messages for invalid input", async () => {
+it("should return a 400 status code and error messages for invalid input", async () => {
     const response = await supertest(app)
     .post('/')
     .send({username: "", password: ""});
     expect(response.status).toBe(400);
     expect(response.body.errors).toEqual(
         expect.arrayContaining([
-            'Username is required',
-            'Password is required'
+            'Username is required.',
+            'Password is required.'
             ])
         );
     });
-});*/
+});
--- Backend/routes/LoginModule.js
+++ Backend/routes/LoginModule.js
@@ -6,7 +6,20 @@ router.post("/", (req, res) => {
 
     const {username, password } = req.body;
 
-    res.status(201).json({ message: `Login successful, username: ${username}, password: ${password}`})
+    const errors = [];
+
+    if (!username.trim()) {
+        errors.push('Username is required.');
+    }
+      if (!password.trim()) {
+        errors.push('Password is required.');
+      }
+
+      if(errors.length > 0){
+        res.status(400).json({ errors });
+      } else {
+        res.status(201).json({ message: `Login successful`})
+      }
 });
 
 module.exports = router;
