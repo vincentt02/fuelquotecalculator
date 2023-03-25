b420f8d4b61ae1b5000d6dca6e2b1b40afd4b59b
Vincent <90940496+vincentt02@users.noreply.github.com>
2023-03-24 22:46:00 -0500

    Fixed server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,4 @@
 const express = require("express");;
-//creates application variable
-const app = express();
-const PORT = 8080;
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 
 //importing the routes
 const ClientProfileManagementRoute = require("./routes/ClientProfileManagementRouter");
@@ -13,10 +7,12 @@ const quoteTable = require('./routes/QuoteTableModule')
 const LoginModuleRoute = require("./routes/LoginModule")
 const registerRoute = require('./routes/Register')
 
+//creates application variable
+const app = express();
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 
 app.use("/api/clientprofilemanagement", ClientProfileManagementRoute);
 app.use('/api', fuelQuote, quoteTable)
@@ -25,13 +21,4 @@ app.use('/Register', registerRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-});
-
-
-
-
-
-//our server is listening on this port for requests
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
 });
