53a6e37c99ce236085e3f7a75b2d76baccf0f0c0
Vincent <90940496+vincentt02@users.noreply.github.com>
2023-03-30 23:02:10 -0500

    Removed extrta code

--- Backend/controllers/QuoteTableController.js
+++ Backend/controllers/QuoteTableController.js
@@ -5,78 +5,6 @@ const fuelquoteModel = require("../models/fuelQuote.js")
 
 var userID = null;
 
-// const quoteHistoryArray = [
-//   {
-//     numG: 20,
-//     address: "2701 Helena St",
-//     date: "05/16/2001",
-//     price: 2.14,
-//     due: 42.8,
-//   },
-//   {
-//     numG: 456,
-//     address: "3552 Travis St",
-//     date: "02/10/2001",
-//     price: 3.24,
-//     due: 1477.4,
-//   },
-//   {
-//     numG: 35,
-//     address: "123 Main St",
-//     date: "01/01/2022",
-//     price: 3.99,
-//     due: 139.65,
-//   },
-//   {
-//     numG: 18,
-//     address: "456 Maple Ave",
-//     date: "02/15/2022",
-//     price: 2.49,
-//     due: 44.82,
-//   },
-//   {
-//     numG: 60,
-//     address: "789 Elm St",
-//     date: "03/31/2022",
-//     price: 4.99,
-//     due: 299.4,
-//   },
-//   {
-//     numG: 40,
-//     address: "101 Oak St",
-//     date: "04/30/2022",
-//     price: 10,
-//     due: 400,
-//   },
-//   {
-//     numG: "invalid",
-//     address: "222 Pine St",
-//     date: "05/31/2022",
-//     price: 1.99,
-//     due: "invalid",
-//   },
-//   {
-//     numG: 10,
-//     address: 999,
-//     date: "06/30/2022",
-//     price: 5.49,
-//     due: 549,
-//   },
-//   {
-//     numG: 25,
-//     address: "444 Maple Ave",
-//     date: "tomorrow",
-//     price: "invalid",
-//     due: 500,
-//   },
-//   {
-//     numG: 30,
-//     address: "555 Oak St",
-//     date: "08/31/2022",
-//     price: "one hundred",
-//     due: 300,
-//   },
-// ];
 const quoteTableSchema = Yup.object({
   numG: Yup.number()
     .typeError("numG must be a number")
@@ -119,7 +47,7 @@ const filterValidQuotes = async (quotes) => {
       await quoteTableSchema.validate(quote);
       validQuotes.push(quote);
     } catch (error) {
-      // console.error("Validation failed:", error.message, quote);
+      console.error("Validation failed:", error.message, quote);
     }
   }
   return validQuotes;
@@ -133,7 +61,6 @@ const getToken = async (req, res) => {
 }
 
 const getQuoteData = async (req, res) => {
-  // const validQuotes = await filterValidQuotes(quoteHistoryArray);
   const query = { userID: userID };
 
   const allData = await fuelquoteModel.find(query);
